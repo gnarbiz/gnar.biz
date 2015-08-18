3945bcef00dc23ee4928242faf4a2274ce964bf9
halabuda <halabuda@gmail.com>
2015-08-18 11:21:25 -0400

    meta description

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta property="og:description" content="<?php echo trim(file_get_contents('inc/meta.description')); ?>" />
     <title>gnar.biz</title>
     <link href='css/gnar.biz.css' rel='stylesheet' type='text/css'>
   </head>
