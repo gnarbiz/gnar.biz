3c782da15ea9d2260e967c98ce11148782348334
halabuda <halabuda@gmail.com>
2015-08-18 09:55:06 -0400

    modularized page elements, responsified ascii container

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+  require_once('inc/fuck.caching.php');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>gnar.biz</title>
+    <link href='css/gnar.biz.css' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+  <div class='gnar'>
+    <div class='biz'>
+    <?php 
+      echo file_get_contents('ascii/gnar.biz.txt');
+    ?>
+    </div>
+  </div>
+  <div class='github'>
+    <a href='//github.com/gnarbiz'>github</a>
+  </div>
+  </body>
+</html>
+
