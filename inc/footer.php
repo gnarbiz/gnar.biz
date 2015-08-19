7c5f7a511f184840d5a271e6c5cc60bf700284d0
halabuda <halabuda@gmail.com>
2015-08-19 17:21:09 -0400

    url.gnar.biz -> gnarl.gnar.biz

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,3 @@
  <div class='github'>
-    [ <a href='//url.gnar.biz'>gnarl</a> | <a href='//github.com/gnarbiz' target='_blank'>github</a> ] = <a href='//gnar.biz'>gnar.biz</a>
+    [ <a href='//gnarl.gnar.biz'>gnarl</a> | <a href='//github.com/gnarbiz' target='_blank'>github</a> ] = <a href='//gnar.biz'>gnar.biz</a>
   </div>
